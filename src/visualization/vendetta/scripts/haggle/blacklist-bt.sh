ea32494bf56739f6d042c37d4785b25f26d74c1d
daniel aldman <daniel.aldman@it.uu.se>
2009-08-31 13:19:51 +0200

    blacklist-eth.sh:
     * New script that is the same as blacklist.sh, except that it includes the change to that file which appeared in dea1421366 (makes the script only accept ethernet blacklisting).
    
    blacklist-bt.sh:
    
     * New script that is the same as blacklist.sh, except the script only accepts bluetooth blacklisting.
    
    Vendetta (demo configuration):
    
     * Added buttons for turning on/off ethernet and bluetooth interfaces specifically.

---
+++ src/visualization/vendetta/scripts/haggle/blacklist-bt.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Script to blacklist mac addresses on Haggle nodes.
+
+function usage() {
+    echo "Usage: $0 IP [ACTION] TYPE MAC [TYPE MAC [...]]"
+    echo "   IP is the IP address of the target node," 
+    echo "   ACTION is either 'up' or 'down', (no action means toggle),"
+    echo "   TYPE is the type of interface (only bluetooth accepted)"
+    echo "   MAC is the mac address to blacklist."
+    exit;
+}
+
+if [ ${#@} -lt 3 ]; then
+    usage;
+fi
+
+cd $(dirname $0)
+
+if [ "$2" == "up" ] ; then
+	ONOFF=' action="remove"'
+	j=2
+elif [ "$2" == "down" ] ; then
+	ONOFF=' action="add"'
+	j=2
+else 
+	ONOFF=''
+	j=1
+fi
+
+TIME=$(date +%s)
+k=0
+l=
+for i in $@ ; do
+	if [ $j -ne "0" ] ; then
+		j=$(echo $j-1 | bc)
+	else
+		if [ $k -ne "1" ] ; then
+			l=$i
+			k=1
+		else
+			k=0
+			if [ "$l" == "bluetooth" ] ; then
+				(
+					echo "<Haggle persistent=\"no\" create_time=\""$TIME".000000\">";
+					echo "	<Attr name=\"Connectivity\">Blacklist</Attr>";
+					echo "	<Connectivity>";
+					echo "		<Blacklist type=\""$l"\""$ONOFF">"$i"</Blacklist>";
+					echo "	</Connectivity>";
+					echo "</Haggle>"
+				) | ./do.sh $1 9697
+			fi
+		fi
+	fi
+done
